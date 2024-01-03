1fadd0730d045a4eee79cd68a309886a15934acb
Heavenboi <nigelzenda98@gmail.com>
2024-01-04 01:41:59 +0200

    added file 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -18,16 +18,14 @@ def get_user_todo():
 
     user = requests.get(api_url_users).json()
 
-    api_url_todos = f'https://jsonplaceholder.typicode.com/todos?userId =
-    {employee_id}'
+    api_url_todos = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
     todos = requests.get(api_url_todos).json()
 
     tasks = sum(1 for todo in todos if todo["completed"])
     task_names = "\n".join(todo["title"]
                            for todo in todos if todo["completed"])
 
-    print(f"Employee {user['name']} is done with tasks
-          ({tasks}/20):\n{task_names}")
+    print(f"Employee {user['name']} is done with tasks({tasks}/20):\n{task_names}")
 
 
 if __name__ == "__main__":
